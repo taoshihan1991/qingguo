3382edd1e7ee9ef180332d56bc1f0d69d97dbea4
陶士涵 <630892807@qq.com>
2015-01-11 20:43:45 +0800

    修改README

--- wish.php
+++ wish.php
@@ -1,15 +1,18 @@
 <?php
+// +----------------------------------------------------------------------
+// | 青果许愿墙
+// +----------------------------------------------------------------------
+// | 新年许愿开始啦
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 陶士涵
+// +----------------------------------------------------------------------
 define('APP_NAME','wish');
 define('APP_PATH','./Wish/');          
 define('ROOT_PATH', str_replace("\\", '/', dirname(__FILE__)));
 define("RUNTIME_PATH", ROOT_PATH . "/Runtime/Wish/");
 define('APP_DEBUG',true);
-
 define('ROOT_URL',strtolower('http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']).'/'));
-
-
-
 require('./ThinkPHP/ThinkPHP.php');
-
-
 ?>
